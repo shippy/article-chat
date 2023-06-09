c9af6a7427bd0e895a8ca7ae910d52a6539718a4
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-06-09 19:51:18 +0200

    Update model

--- app/models/chat.py
+++ app/models/chat.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Literal
 
 class ChatMessage(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     content: str
+    originator: Literal["user", "bot"] = Field(default="user")
     user_id: int
     created_at: Optional[datetime] = Field(default_factory=datetime.now)
     deleted_at: Optional[datetime] = Field(default=None)
--- app/models/user.py
+++ app/models/user.py
@@ -4,5 +4,5 @@ from pydantic import EmailStr
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     email: EmailStr = Field(unique=True)
-    cognito_jwt: str
-    disabled: bool = False
+    cognito_sub_id: str = Field(..., unique=True)
+    is_disabled: bool = Field(default=False)
