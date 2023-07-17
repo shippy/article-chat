219670cf3208d53bfd1645ef7eedd8306fb6a637
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-07-17 19:18:13 +0200

    Add basic Vue app (broken)

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar.vue'
+</script>
+
+<template>
+  <div id="app">
+    <sidebar/>
+    <router-view/>
+  </div>
+</template>
---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Uploaded Documents</h2>
+      <ul>
+        <li v-for="document in documents" :key="document.id">
+          {{ document.name }}
+          <button @click="startChat(document.id)">Start Chat</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import apiService from '../services/api.service'
+  
+  export default {
+    data() {
+      return {
+        documents: []
+      }
+    },
+    async created() {
+      this.documents = await apiService.getDocuments();
+    },
+    methods: {
+      startChat(docId) {
+        // Logic to start a chat
+      }
+    }
+  }
+  </script>
+  
