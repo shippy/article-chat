e1d84a528083407b9b11d611ef87c6ab035537a7
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-07-18 19:24:12 +0200

    Adapt to changes in username/pw storage

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,14 +1,26 @@
 from dotenv import load_dotenv
+import json
 import os
 from sqlmodel import Session, create_engine
-from typing import Generator
+from typing import Dict, Generator
 
 _ = load_dotenv()
 
-# engine = create_engine("sqlite:///database.db", echo=True)
+credentials: Dict[str, str] = json.loads(os.environ.get("PGVECTOR_CREDENTIALS", '"{}"'))
+try:
+    POSTGRES_USER = credentials["username"]
+    POSTGRES_PASSWORD = credentials["password"]
+except KeyError as e:
+    raise KeyError(
+        "Please set the environment variable PGVECTOR_CREDENTIALS to a JSON object "
+        "with keys 'username' and 'password'"
+    ) from e
+
 connection_string = (
-    f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}"
-    f"@database:5432/{os.environ.get('POSTGRES_DB')}"
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{os.environ.get('POSTGRES_URL', 'database')}"
+    f":{os.environ.get('POSTGRES_PORT', '5432')}"
+    f"/{os.environ.get('POSTGRES_DB', 'postgres')}"
 )
 engine = create_engine(connection_string, echo=True)
 
