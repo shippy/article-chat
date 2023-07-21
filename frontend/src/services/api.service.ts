d42b78b1b92567e38cf692deffa450f5bd6ff039
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-07-21 07:57:30 +0200

    Add withCredentials: true to every API request

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -4,23 +4,29 @@ const API_URL: String = 'https://api.journalarticle.chat'
 
 export default {
   async uploadDocument(file: File) {
-    const formData = new FormData();
-    formData.append('file', file);
+    const formData = new FormData()
+    formData.append('file', file)
     await axios.post(`${API_URL}/upload_document`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
-    });
+    })
   },
   async getDocuments() {
-    const response = await axios.get(`${API_URL}/documents`, { withCredentials: true });
-    return response.data;
+    const response = await axios.get(`${API_URL}/documents`, { withCredentials: true })
+    return response.data
   },
   async getChat(docId: Number, chatId: Number) {
-    const response = await axios.get(`${API_URL}/document/${docId}/chat/${chatId}`);
-    return response.data;
+    const response = await axios.get(`${API_URL}/document/${docId}/chat/${chatId}`, {
+      withCredentials: true
+    })
+    return response.data
   },
   async sendMessage(docId: Number, chatId: Number, message: String) {
-    await axios.post(`${API_URL}/document/${docId}/chat/${chatId}/submit`, { message });
+    await axios.post(
+      `${API_URL}/document/${docId}/chat/${chatId}/submit`,
+      { message },
+      { withCredentials: true }
+    )
   }
 }
