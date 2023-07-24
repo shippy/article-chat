7775558c6e6af4a155cfd7932d33aacc2a3fbae6
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-07-24 18:57:18 +0200

    Fix TS issues + handle errors + refresh docs

--- frontend/src/components/FileUpload.vue
+++ frontend/src/components/FileUpload.vue
@@ -1,21 +1,36 @@
 <template>
     <input ref="fileInput" :disabled="isUploading" type="file" @change="uploadFile" />
+    <p v-if="uploadError">{{ uploadError }}</p>
 </template>
   
 <script setup lang="ts">
 import { ref } from 'vue'
 import apiService from '../services/api.service'
+import { useDocumentsStore } from '@/stores/documents';
 
 const isUploading = ref(false)
-const fileInput = ref(null)
+const fileInput = ref<HTMLInputElement | null>(null)
+const uploadError = ref<string | null>(null)
+const store = useDocumentsStore();
 
 const uploadFile = async (event: Event) => {
-    const file = (event.target as HTMLInputElement).files[0];
-    isUploading.value = true
-    const response = await apiService.uploadDocument(file);
-    isUploading.value = false
-    fileInput.value.value = null;
-
-    return response;
+    const fileInputEl = event.target as HTMLInputElement;
+    const file = fileInputEl.files ? fileInputEl.files[0] : null;
+    if (file) {
+        try {
+            isUploading.value = true
+            const response = await apiService.uploadDocument(file);
+            isUploading.value = false
+            if (fileInput.value) {
+                fileInput.value.value = '';
+            }
+            uploadError.value = null
+            store.fetchDocuments();
+            return response;
+        } catch (err) {
+            uploadError.value = (err as Error).message || "An error occurred during file upload"
+            isUploading.value = false
+        }
+    }
 };
 </script>
