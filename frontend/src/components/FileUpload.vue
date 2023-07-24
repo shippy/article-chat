e6df6c25f5217e8082f5a548294ba17cd2ea595a
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-07-24 18:41:55 +0200

    Make upload button reactive

--- frontend/src/components/FileUpload.vue
+++ frontend/src/components/FileUpload.vue
@@ -1,18 +1,21 @@
 <template>
-    <input type="file" @change="uploadFile" />
+    <input ref="fileInput" :disabled="isUploading" type="file" @change="uploadFile" />
 </template>
   
-<script lang="ts">
+<script setup lang="ts">
+import { ref } from 'vue'
 import apiService from '../services/api.service'
-// TODO: Use document store instead?
-export default {
-    methods: {
-        async uploadFile(event: any) {
-            const file = event.target.files[0];
-            const response = await apiService.uploadDocument(file);
-            return response
-        }
-    }
-}
+
+const isUploading = ref(false)
+const fileInput = ref(null)
+
+const uploadFile = async (event: Event) => {
+    const file = (event.target as HTMLInputElement).files[0];
+    isUploading.value = true
+    const response = await apiService.uploadDocument(file);
+    isUploading.value = false
+    fileInput.value.value = null;
+
+    return response;
+};
 </script>
-  
