29008eb39b237d7bf363880d32a9a5985e85df37
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-07-28 00:06:10 +0200

    Punch up the copy

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,24 +2,24 @@
   <div class="landing-page">
     <section class="hero">
       <h1>Chat with Journal Articles</h1>
-      <p>Upload your documents and discuss them in a unique chat interface!</p>
+      <p>Upload your documents and talk to them in a unique chat interface!</p>
       <p><a href="https://www.linkedin.com/in/simonpodhajsky">Contact Simon for beta access.</a></p>
     </section>
     <section class="features">
       <div class="feature">
         <div class="illustration"><img src="/feature_upload.png" /></div>
         <h2>Upload Your Documents</h2>
-        <p>Upload your documents and convert them into vector embeddings.</p>
+        <p>Upload and convert your PDFs into vector embeddings.</p>
       </div>
       <div class="feature">
         <div class="illustration"><img src="/feature_chat.png" /></div>
         <h2>Start a Chat</h2>
-        <p>Start a chat and discuss your documents in a structured way.</p>
+        <p>Chat up your documents in a structured way.</p>
       </div>
       <div class="feature">
         <div class="illustration"><img src="/feature_search.png" /></div>
-        <h2>Advanced Search <em>(coming soon...)</em></h2>
-        <p>Use our advanced search to find specific documents parts by their meaning.</p>
+        <h2>Advanced Search <em>(coming soon!)</em></h2>
+        <p>Find specific document parts by their text or meaning.</p>
       </div>
     </section>
   </div>
