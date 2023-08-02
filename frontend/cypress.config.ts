6be9d07bca9494d194ca0d81aaf0e876d42602bd
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-08-02 02:36:11 +0200

    Test basic login flow on frontend

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "cypress";
+import 'dotenv/config';
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 
+  env: {
+    'cognito_domain': 'https://auth.journalarticle.chat',
+    'cognito_username': process.env.CYPRESS_cognito_username,
+    'cognito_password': process.env.CYPRESS_cognito_password,
+  },
+  
   component: {
     devServer: {
       framework: "vue",
--- frontend/cypress/e2e/AppSpec.cy.ts
+++ frontend/cypress/e2e/AppSpec.cy.ts
@@ -1,5 +1,35 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Logged-out Homepage spec', () => {
+  it('loads with blurb', () => {
     cy.visit('http://localhost:5173')
+    cy.get('img')
+  }),
+  it('has clickable login link', () => {
+    cy.visit('http://localhost:5173')
+    cy.contains('Log in').click()
   })
+})
+
+describe('Logged-in Homepage spec', () => {
+  beforeEach(function() {
+    cy.loginByCognito(Cypress.env('cognito_username'), Cypress.env('cognito_password'))
+  })
+
+  // it('shows sidebar and upload button', function() {
+  //   cy.contains('Uploaded Documents')
+  //   cy.get('#file_upload>input[type="file"]')
+  // })
+
+  it('navigates to chat and retrieves messages', function() {
+    cy.visit('/start')
+    // TODO: Universalize
+    cy.get('a:contains(Chat 10)').click()
+    cy.contains('Chat 10 for Document')
+    cy.get('.ai').should('be.visible')
+  })
+
+  // it('uploads a file', function() {})
+
+  // it('starts a chat', function() {})
+
+  // it('sends a message and receives a response', function() {})
 })
